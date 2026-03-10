cb030e3ee8c6e9b3afcd98bd863490f1688593e8
Carlxszin <crlosmodder@gmail.com>
2026-03-10 17:07:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
 const CACHE_NAME = 'recargafast-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const ASSETS = ['index.html'];
 
-// Instala o Service Worker e armazena arquivos básicos em cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Responde às requisições
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
